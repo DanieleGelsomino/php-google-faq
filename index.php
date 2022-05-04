821d04d6592a61d0a63aeb0d0a93a9ab337edf0a
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-05-05 00:41:08 +0200

    done

--- index.php
+++ index.php
@@ -14,8 +14,14 @@
 </head>
 <body>
     <div class="header">
+        <div class="left-side">
         <img class="logo" src="https://www.edigitalagency.com.au/wp-content/uploads/google-logo-png-transparent-background-large-new.png" alt="logo-google">
-        <h2 class="title">Privacy e termini</h2>
+        <h1 class="title">Privacy e termini</h1>
+        </div>
+  <div class="right-side">
+      <img class="icon" src="img/svgexport-4.png" alt="">
+      <h1 class="account">D</h1>
+  </div>
     </div>
     <div class="container-faq">
         <?php
