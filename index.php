10a986e44b759c90d65daef9e9f28989e3a2300c
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-05-04 17:00:20 +0200

    add the visualization of the data in the page

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 //phpinfo();
 
-include 'data_faq.php';
+include ('data_faq.php');
+
+foreach ($faq as $key => $single_faq) {
+    echo( '<h2>' . $single_faq['question'] . '</h2>');
+
+    foreach($single_faq['answer'] as $answer) {
+        echo('<p>' . $answer . '</p>');
+    }
+}
 
 ?>
