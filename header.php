2fc59c6d4125fb702c46452dce1228c34b29ed41
Daniele Gelsomino <96696967+DanieleGelsomino@users.noreply.github.com>
2022-05-05 12:45:54 +0200

    add include template and header php

---
+++ header.php
@@ -0,0 +1,10 @@
+<div class="header">
+        <div class="left-side">
+        <img class="logo" src="https://www.edigitalagency.com.au/wp-content/uploads/google-logo-png-transparent-background-large-new.png" alt="logo-google">
+        <h1 class="title">Privacy e termini</h1>
+        </div>
+        <div class="right-side">
+        <img class="icon" src="img/svgexport-4.png" alt="">
+        <h1 class="account">D</h1>
+        </div>
+    </div>
--- index.php
+++ index.php
@@ -1,28 +1,14 @@
 
 
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;300;400;600;700;800;900&family=Open+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <title>Google FAQ</title>
-</head>
+<html lang="it">
+<?php
+include ('template.php');
+?>
 <body>
-    <div class="header">
-        <div class="left-side">
-        <img class="logo" src="https://www.edigitalagency.com.au/wp-content/uploads/google-logo-png-transparent-background-large-new.png" alt="logo-google">
-        <h1 class="title">Privacy e termini</h1>
-        </div>
-  <div class="right-side">
-      <img class="icon" src="img/svgexport-4.png" alt="">
-      <h1 class="account">D</h1>
-  </div>
-    </div>
+<?php
+include ('header.php');
+?>
     <div class="container-faq">
         <?php
 
